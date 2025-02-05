883c776d63d2dbeea04f4b97f9520424eb821226
brittany <brownbb1990@gmail.com>
2025-02-05 06:10:16 +0000

    added new field

--- server/store/models.py
+++ server/store/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
     active = models.BooleanField()
     discount_percent = models.IntegerField()
     inventory_count = models.IntegerField()
-
+    product_we_love = models.BooleanField(default=false)
     @property
     def discount_saving(self):
         return Decimal(round(float(self.price) * (self.discount_percent / 100), 2))
